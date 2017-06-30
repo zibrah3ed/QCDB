c5d4d6cef439f6565fac43bc75e0ceefe46b56ad
Tyson Funk <zibrah3ed@gmail.com>
2017-06-30 07:55:03 -0500

    Add joins
    Added joins to select data from aggregate and job tables. Functionailty is broken

--- table.php
+++ table.php
@@ -65,13 +65,17 @@
       if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
       }
-      echo "Connected successfully";
+      //echo "Connected successfully";
         // Get variable from form submit
         $jobNum = $_GET["jobNum"];
-        $sql = "SELECT Jobs_job_id, aggPercent, Aggregate_agg_id, sieve2inch, sieve15inch,
-                sieve1inch, sieve34inch, sieveNo4, sieve12inch, sieve38inch,
-                sieveNo8, sieveNo16, sieveNo30, sieveNo50,sieveNo100,sieveNo200
-                FROM gradations where sieveNo4 > 50";
+        $sql = "SELECT Jobs_job_id, aggPercent, Aggregate_agg_id, sieve2inch, sieve15inch, sieve1inch,
+                  sieve34inch, sieveNo4, sieve12inch, sieve38inch, sieveNo8, sieveNo16, sieveNo30,
+                  sieveNo50,sieveNo100,sieveNo200, Jobs.KossProjNum, aggregate.aggLocalName,
+                  aggregate.aggProducer
+                  FROM Jobs
+                  INNER JOIN gradations ON Jobs.job_id = gradations.Jobs_job_id
+                  INNER JOIN aggregate ON gradations.aggregate_agg_id = aggregate.agg_id
+                  where Jobs.kossProjNum = '$jobNum'";
         $result = $conn->query($sql);
         echo "<div class='result-table container'>";
         echo "<div class='table-responsive'>";
@@ -103,8 +107,8 @@
         //you can add thead tag here if you want your table to have column headers
           while($rowitem = mysqli_fetch_array($result)) {
             echo "<tr>";
-            echo "<td>" . $rowitem['Jobs_job_id'] . "</td>";
-            echo "<td>" . $rowitem['Aggregate_agg_id'] . "</td>";
+            echo "<td>" . $jobNum . "</td>"; // Change later to match returned data
+            echo "<td>" . htmlspecialchars($rowitem['aggregate.aggLocalName']) . "</td>";
             echo "<td>" . $rowitem['aggPercent'] . "</td>";
             echo "<td>" . $rowitem['sieve2inch'] . "</td>";
             echo "<td>" . $rowitem['sieve15inch'] . "</td>";
