4e76f3385d8c4e330b14f5c33a045945b1bcbfad
Tyson Funk <zibrah3ed@gmail.com>
2017-06-30 13:20:04 -0500

    Broken table.php

--- opendb.php
+++ opendb.php
@@ -1,7 +1,9 @@
 <?php // Create connection
-$conn = new mysqli($servername, $username, $password, $dbName);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+  $conn = new mysqli($servername, $username, $password, $dbName);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } else {
+    echo("<br />");
+  }
 ?>
--- table.php
+++ table.php
@@ -53,81 +53,84 @@
 <!-- End Banner -->
 
 
-  <!-- PHP Scripts -->
-  <?php
-        include 'config.php';
-        include 'opendb.php';
-
-        // Get variable from form submit
-        $jobNum = $_GET["jobNum"];
-        $sql = "SELECT Jobs_job_id, aggPercent, Aggregate_agg_id, sieve2inch, sieve15inch, sieve1inch,
-                  sieve34inch, sieveNo4, sieve12inch, sieve38inch, sieveNo8, sieveNo16, sieveNo30,
-                  sieveNo50,sieveNo100,sieveNo200, Jobs.KossProjNum, aggregate.aggLocalName,
-                  aggregate.aggProducer
-                  FROM Jobs
-                  INNER JOIN gradations ON Jobs.job_id = gradations.Jobs_job_id
-                  INNER JOIN aggregate ON gradations.aggregate_agg_id = aggregate.agg_id
-                  where Jobs.kossProjNum = '$jobNum'";
-        $result = $conn->query($sql);
-        echo "<div class='result-table container'>";
-        echo "<div class='table-responsive'>";
-        echo "<table class='table-test table'>"; //begin table tag...
-        if ($result->num_rows > 0) {
-          echo "<tr>
-            <th>
-            Job
-            </th>
-            <th>
-            Agg Name
-            </th>
-            <th>
-            Producer
-            </th>
-            <th>
-            Agg Percent
-            </th>
-            <th>2.0</th><th>1.5</th>
-            <th>1.0</th>
-            <th>3/4</th>
-            <th>1/2</th>
-            <th>3/8</th>
-            <th>No.4</th>
-            <th>No.8</th>
-            <th>No.16</th>
-            <th>No.30</th>
-            <th>No.50</th>
-            <th>No.100</th>
-            <th>No.200</th>
-          </tr>";
-        //you can add thead tag here if you want your table to have column headers
-          while($rowitem = mysqli_fetch_array($result)) {
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($rowitem['KossProjNum']) . "</td>";
-            echo "<td>" . htmlspecialchars($rowitem['aggLocalName']) . "</td>";
-            echo "<td>" . htmlspecialchars($rowitem['aggProducer']) . "</td>";
-            echo "<td>" . $rowitem['aggPercent'] . "</td>";
-            echo "<td>" . $rowitem['sieve2inch'] . "</td>";
-            echo "<td>" . $rowitem['sieve15inch'] . "</td>";
-            echo "<td>" . $rowitem['sieve1inch'] . "</td>";
-            echo "<td>" . $rowitem['sieve34inch'] . "</td>";
-            echo "<td>" . $rowitem['sieve12inch'] . "</td>";
-            echo "<td>" . $rowitem['sieve38inch'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo4'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo8'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo16'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo30'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo50'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo100'] . "</td>";
-            echo "<td>" . $rowitem['sieveNo200'] . "</td>";
-            echo "</tr>";
-          }
-            } else {
-          echo "<b>No results - Check Job Number Formatting.</b>";
+<!-- PHP Scripts -->
+<?php
+    $servername = "localhost";//localh.dev @ Home
+    $username = "root";
+    $password = "pittsburg";
+    $dbName = "QCDB";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbName);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    //echo "Connected successfully";
+      // Get variable from form submit
+      $jobNum = $_GET["jobNum"];
+      $sql = "SELECT Jobs_job_id, aggPercent, Aggregate_agg_id, sieve2inch, sieve15inch, sieve1inch,
+                sieve34inch, sieveNo4, sieve12inch, sieve38inch, sieveNo8, sieveNo16, sieveNo30,
+                sieveNo50,sieveNo100,sieveNo200, Jobs.KossProjNum, aggregate.aggLocalName,
+                aggregate.aggProducer
+                FROM Jobs
+                INNER JOIN gradations ON Jobs.job_id = gradations.Jobs_job_id
+                INNER JOIN aggregate ON gradations.aggregate_agg_id = aggregate.agg_id
+                where Jobs.kossProjNum = '$jobNum'";
+      $result = $conn->query($sql);
+      echo "<div class='result-table container'>";
+      echo "<div class='table-responsive'>";
+      echo "<table class='table-test table'>"; //begin table tag...
+      if ($result->num_rows > 0) {
+        echo "<tr>
+          <tr>Job</th>
+          <th>Agg Name</th>
+          <th>Producer</th>
+          <th>Agg Percent</th
+          <th>2.0</th><th>1.5</th>
+          <th>1.0</th>
+          <th>3/4</th>
+          <th>1/2</th>
+          <th>3/8</th>
+          <th>No.4</th>
+          <th>No.8</th>
+          <th>No.16</th>
+          <th>No.30</th>
+          <th>No.50</th>
+          <th>No.100</th>
+          <th>No.200</th>
+        </tr>";
+      //you can add thead tag here if you want your table to have column headers
+        while($rowitem = mysqli_fetch_array($result)) {
+          echo "<tr>";
+          echo "<td>" . htmlspecialchars($rowitem['KossProjNum']) . "</td>";
+          echo "<td>" . htmlspecialchars($rowitem['aggLocalName']) . "</td>";
+          echo "<td>" . htmlspecialchars($rowitem['aggProducer']) . "</td>";
+          echo "<td>" . $rowitem['aggPercent'] . "</td>";
+          echo "<td>" . $rowitem['sieve2inch'] . "</td>";
+          echo "<td>" . $rowitem['sieve15inch'] . "</td>";
+          echo "<td>" . $rowitem['sieve1inch'] . "</td>";
+          echo "<td>" . $rowitem['sieve34inch'] . "</td>";
+          echo "<td>" . $rowitem['sieve12inch'] . "</td>";
+          echo "<td>" . $rowitem['sieve38inch'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo4'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo8'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo16'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo30'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo50'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo100'] . "</td>";
+          echo "<td>" . $rowitem['sieveNo200'] . "</td>";
+          echo "</tr>";
         }
-        echo "</table>";
-        echo "</div></div>"; //end table tag
-        ?>
-  <!-- End PHP -->
+          } else {
+        echo "0 results";
+      }
+      echo "</table>";
+      echo "</div></div>"; //end table tag
+
+      // Close Connection
+      $conn->close();
+      ?>
+<!-- End PHP -->
   <div class="container">
     <div class="result-table-two">
       <div class="row">
