d4ff612b29c53016f52f9675c1beb2aba7ff5993
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-06-29 20:32:19 -0500

    Change connection string in table.php

--- table.php
+++ table.php
@@ -6,29 +6,41 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>QC DB Front End</title>
   <link rel="stylesheet" href="styles.css">
+
+  <!-- Bootstrap -->
+  <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+      </script>
+
 </head>
 <body>
-
   <!-- Banner at top of page -->
-  <div class="topOfPage">
-    <h1>Under Construction</h1>
-  </div>
 
-  <br>
 
   <!-- PHP Scripts -->
+  <?/*php print(Date("l F d, Y")); */?>
   <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "pittsburg";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
+      $servername = "localh.dev";
+      $username = "root";
+      $password = "pittsburg";
 
-     Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-    ?>
+      // Create connection
+      $conn = new mysqli($servername, $username, $password);
+
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+      echo "Connected successfully";
+  ?>
+  <!-- End PHP -->
+
+
 </body>
 </html>
