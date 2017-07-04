5327c1e3f1421f120ac3cf3c2d8cd390d407cbae
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-07-03 19:53:56 -0500

    Add comments to code

--- createTable.php
+++ createTable.php
@@ -3,7 +3,16 @@ function makeTable($agg_Type){
   include 'config.php';
   include 'opendb.php';
 
+  // Array of column names of sql database
+  $sieveRowArray = array("sieve2inch", "sieve15inch", "sieve1inch",
+            "sieve34inch", "sieveNo4", "sieve12inch", "sieve38inch", "sieveNo8", "sieveNo16", "sieveNo30",
+            "sieveNo50","sieveNo100","sieveNo200");
+  // Array if sieve labels
+  $sieveDescArray = array('2.0"','1.5"','1.0"','3/4"','1/2"','3/8"','#4','#8','#16','#30','#50','#100','#200');
+  // Get job number from url on form submit
   $jobNum = $_GET["jobNum"];
+
+  // SQL select statement using a join between jobs, aggregates, and gradations
   $sql = "SELECT Jobs_job_id, aggPercent, Aggregate_agg_id, sieve2inch, sieve15inch, sieve1inch,
             sieve34inch, sieveNo4, sieve12inch, sieve38inch, sieveNo8, sieveNo16, sieveNo30,
             sieveNo50,sieveNo100,sieveNo200, Jobs.KossProjNum, aggregate.aggLocalName,
@@ -12,6 +21,8 @@ function makeTable($agg_Type){
             INNER JOIN gradations ON Jobs.job_id = gradations.Jobs_job_id
             INNER JOIN aggregate ON gradations.aggregate_agg_id = aggregate.agg_id
             where Jobs.kossProjNum = '$jobNum' AND aggregate.aggType = '$agg_Type'";
+
+  // Establish connection
   $result = $conn->query($sql);
 
   if ($result->num_rows > 0) {
